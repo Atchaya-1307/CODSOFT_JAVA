02a00e5c05583ab6ce3429e62d8c0b579c2bdbc2
ATCHAYA <atchayapandishelvam@gmail.com>
2024-10-06 12:38:08 +0530

    Add files via upload

---
+++ Studentmark.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+public class Studentmark {
+       
+       public static char calculateGrade(double average) {
+        if (average >= 90) {
+            return 'A';
+        } else if (average >= 80) {
+            return 'B';
+        } else if (average >= 70) {
+            return 'C';
+        } else if (average >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int subjects = scanner.nextInt();
+        double[] marks = new double[subjects];
+
+        for (int i = 0; i < subjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextDouble();
+        }
+
+        
+        double totalMarks = 0;
+        for (double mark : marks) {
+            totalMarks += mark;
+        }
+
+       
+        double averagePercentage = totalMarks / subjects;
+
+        
+        char grade = calculateGrade(averagePercentage);
+
+        
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
